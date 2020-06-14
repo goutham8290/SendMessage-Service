e48d358e43c71301a11f1735f770a303d256845b
goutham8290 <48564537+goutham8290@users.noreply.github.com>
2020-06-14 21:40:32 +0200

    Add files via upload

---
+++ SendMessageService/src/main/java/com/sendmessage/SendMessageApplication.java
@@ -0,0 +1,25 @@
+package com.sendmessage;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SendMessageApplication extends Application<SendMessageConfiguration> {
+
+
+    private static Logger LOGGER = LoggerFactory.getLogger(SendMessageApplication.class);
+
+    public static void main(String[] args) throws Exception {
+
+        new SendMessageApplication().run(args);
+
+    }
+
+    public void run(SendMessageConfiguration sendMessageConfiguration, Environment environment) throws Exception {
+
+        LOGGER.info(  sendMessageConfiguration.getDefaultName());
+        LOGGER.info("Application Start");
+
+    }
+}
